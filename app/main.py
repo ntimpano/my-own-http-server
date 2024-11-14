eda7768dcdc27b7805693f03d774874f6c4b7c66
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-14 00:24:54 -0300

    add response 200

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
+    
+    http_response: ("HTTP/1.1 200 OK\r\n\r\n")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()
+    server_socket.sendall(http_response.encode())
+
+
 
 
 if __name__ == "__main__":
