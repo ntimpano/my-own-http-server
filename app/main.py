18db49caa32a40bceff82182e53fabc7b0fc3c96
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-14 00:25:42 -0300

    add response 200

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import socket  # noqa: F401
 def main():
     print("Logs from your program will appear here!")
     
-    http_response: ("HTTP/1.1 200 OK\r\n\r\n")
+    http_response = ("HTTP/1.1 200 OK\r\n\r\n")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept()
     server_socket.sendall(http_response.encode())
