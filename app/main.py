03e6c186eae5890337183df3a241b62333ccc458
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-14 00:37:35 -0300

    add response 200

--- app/main.py
+++ app/main.py
@@ -4,9 +4,12 @@ import socket  # noqa: F401
 def main():
     print("Logs from your program will appear here!")
     
+    
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
-    server_socket.accept()[0](b"HTTP/1.1 200 OK\r\n\r\n")
-
+    client_socket, client_address = server_socket.accept()
+    http_response = b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"
+    client_socket.sendall(http_response)
+    client_socket.close()
+    
 if __name__ == "__main__":
     main()
