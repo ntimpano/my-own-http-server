06220954b995c88949d6c9f5728cf13139b6f7ff
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-14 18:31:24 -0300

    add body to response

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import re
 def main():
     
     valid_path = ['/', '/index.html', '/echo/.*$']
-    http_response_ok = b"HTTP/1.1 200 OK\r\n\r\n"
+    
     http_response_failed = b"HTTP/1.1 404 Not Found\r\n\r\n"
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept()
@@ -14,11 +14,15 @@ def main():
     request = split_msg[0]
     split_request = request.split()
     path = split_request[1]
-    print(path)
+    response_body = path.replace('/echo/', '')
+    encode_body = response_body.encode()
     valid = False
+    http_response_ok = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"
+    final_res = http_response_ok + encode_body
+    print(final_res)
     for item in valid_path:
         if re.match(item, path):
-            client_socket.send(http_response_ok)
+            client_socket.send(final_res)
             valid = True
             break
 
