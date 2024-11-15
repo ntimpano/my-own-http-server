a2bceaab4ef9e4f0ea8298dffe74a38aa4e6f7e7
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-15 14:59:03 -0300

    add support for /files endpoint - OK

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import socket
 import threading
+import sys
+import os
 
-valid_path = ['/', '/echo', '/index.html', '/user-agent']
+valid_path = ['/', '/echo', '/index.html', '/user-agent', '/files']
+args = sys.argv
+
 
 def client_handle(client_socket):
     try:
@@ -11,6 +15,7 @@ def client_handle(client_socket):
 
         path = split_req[1]
 
+        
         if 'echo' in path:
             path = path.replace('/echo/', '')
             response_body = path
@@ -23,6 +28,22 @@ def client_handle(client_socket):
             response_ok = (f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{response_body}').encode()
         elif path == '/':
             response_ok = (f'HTTP/1.1 200 OK\r\n\r\n').encode()
+        elif 'files' in path:
+            dir_index = args.index('--directory') + 1
+            dir = args[dir_index]
+            file = path.replace('/files/', '')
+            full_path = dir + file
+            if os.path.isfile(full_path):
+                content_length = os.stat(full_path).st_size
+                content_type = 'application/octet-stream'
+                file_content = open(full_path, 'r')
+                content = file_content.read()
+                response_body = content
+                response_ok = (f'HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\nContent-Length: {content_length}\r\n\r\n{response_body}').encode()
+                print('SE CUMPLIO ESTA CONDICION')
+                file_content.close()
+            else:
+                response_ok = ('HTTP/1.1 404 Not Found\r\n\r\n').encode()
         else:
             path = path.replace('/', '')
             response_body = path
@@ -31,7 +52,7 @@ def client_handle(client_socket):
         
         response_failed = ('HTTP/1.1 404 Not Found\r\n\r\n').encode()
 
-        if split_req[1] in valid_path or split_req[1].startswith('/echo'):
+        if split_req[1] in valid_path or split_req[1].startswith('/echo') or split_req[1].startswith('/files'):
             client_socket.send(response_ok)
         else:
             client_socket.send(response_failed)
