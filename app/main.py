942e8f424c1a8b2f3587fbf0dbe5e3468060e70a
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-15 11:40:49 -0300

    Restoring old source code

--- app/main.py
+++ app/main.py
@@ -12,70 +12,52 @@ def main():
     msg = client_socket.recv(4096) 
     decoded_msg = msg.decode()
     split_msg = decoded_msg.split('\r\n')
-    request = split_msg[0]
-    split_request = request.split()
-    path = split_request[1]
+
+    if len(split_msg) > 2:
+         path = split_msg[2].split()[1]
+         response_body = path
+         encode_body = response_body.encode()
+         length_content = len(response_body)
+         encode_body = response_body.encode()
+    
+    elif len(split_msg) > 1 < 2: 
+         request = split_msg[0]
+         split_request = request.split()
+         path = split_request[1]
+         response_body = path.replace('/echo/', '')
+         encode_body = response_body.encode()
+         length_content = len(response_body)
+
+
+    #request = split_msg[0]
+    #split_request = request.split()
+    #path = split_request[1]
+    #response_body = path.replace('/echo/', '')
+    #encode_body = response_body.encode()
+    #length_content = len(response_body)
+    #usr_agent_path = split_msg[2].split()[1]
+    #usr_length = len(usr_agent_path)
+    #usr_agent_path_encode = usr_agent_path.encode()
+
+    
+    #print(f'hola nahuel {split_request}')
+    #print(f'hola nahuel {length_content}')
+
+    final_res = (
+         f'HTTP/1.1 200 OK\r\n'
+         f'Content-Type: text/plain\r\n'
+         f'Content-Length: {length_content}\r\n'
+         f'\r\n'
+    ).encode() + encode_body
+
 
     valid = False
-
-    if path == '/user-agent':
-        response_body = split_msg[3]
-        response_body = response_body.split()
-        response_body = response_body[1]
-        encode_body = response_body.encode()
-        length_content = len(response_body)
-        final_res = (
-            f'HTTP/1.1 200 OK\r\n'
-            f'Content-Type: text/plain\r\n'
-            f'Content-Length: {length_content}\r\n'
-            f'\r\n'
-            ).encode() + encode_body
-        valid = True
-    elif path.startswith('/echo'):
-        response_body = path.replace('/echo/', '')
-        encode_body = response_body.encode()
-        length_content = len(response_body)
-        valid = True
-        final_res = (
-            f'HTTP/1.1 200 OK\r\n'
-            f'Content-Type: text/plain\r\n'
-            f'Content-Length: {length_content}\r\n'
-            f'\r\n'
-            ).encode() + encode_body
-    elif path == '/index.html':
-        response_body = path.replace('/', '')
-        encode_body = response_body.encode()
-        length_content = len(response_body)
-        final_res = (
-            f'HTTP/1.1 200 OK\r\n'
-            f'Content-Type: text/plain\r\n'
-            f'Content-Length: {length_content}\r\n'
-            f'\r\n'
-            ).encode() + encode_body
-        valid = True
-    elif path == '/':
-        response_body = path.replace('/', '')
-        encode_body = response_body.encode()
-        length_content = len(response_body)
-        final_res = (
-            f'HTTP/1.1 200 OK\r\n'
-            f'Content-Type: text/plain\r\n'
-            f'Content-Length: {length_content}\r\n'
-            f'\r\n'
-            ).encode() + encode_body
-        valid = True
-    else:
-        valid = False
-
-
-
-
-  
     for item in valid_path:
         if re.match(item, path):
             client_socket.send(final_res)
             valid = True
             break
+            
 
     if not valid:
             client_socket.send(http_response_failed)
